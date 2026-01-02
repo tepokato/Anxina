d18ab8a07b681489ee729baa930ef34605eb2366
tepokato <tepokato@gmail.com>
2026-01-02 16:48:11 -0600

    Revert "Add Bootstrap 5.3.8 CSS CDN link to site pages and post template"

--- scripts/build_posts.py
+++ scripts/build_posts.py
@@ -70,7 +70,6 @@ HTML_TEMPLATE = """<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>ANXiNA · {title}</title>
   <link rel="stylesheet" href="../assets/css/style.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
   <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
